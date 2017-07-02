107ce8236edf1124fb57625b02271c0b0a1f6368
kornel <lapacz.kornel@gmail.com>
2017-07-02 22:25:16 +0200

    use strict

--- minlib.js
+++ minlib.js
@@ -1,62 +1,79 @@
-// querySelector
+(function () {
+  "use strict";
 
-let sel = (selectors, baseElement) => {
-  
-  if(!baseElement){
-    baseElement = document;
+  // variables
+
+  let element,
+      el;
+
+  // querySelector
+
+  const sel = (selectors, baseElement) => {
+
+    if(!baseElement){
+      baseElement = document;
+    }
+
+    return baseElement.querySelector(selectors);
   }
-  
-  return baseElement.querySelector(selectors);
-}
 
-// querySelectorAll
+  // querySelectorAll
 
-let all = (selectors, baseElement) => {
-  let toReturn;
-  
-  if(!baseElement){
-    baseElement = document;
-  }
-  if(Array.isArray(selectors)){
-    toReturn = []
-    
-    for(element of selectors){
-      for(el of baseElement.querySelectorAll(element)){
-        toReturn.push(el);
+  const all = (selectors, baseElement) => {
+    let toReturn;
+
+    if(!baseElement){
+      baseElement = document;
+    }
+    if(Array.isArray(selectors)){
+      toReturn = [];
+
+      for(element of selectors){
+        for(el of baseElement.querySelectorAll(element)){
+          toReturn.push(el);
+        } 
       } 
-    } 
-  } else {
-    toReturn = baseElement.querySelectorAll(selectors)
+    } else {
+      toReturn = baseElement.querySelectorAll(selectors);
+    }
+
+    return toReturn;
   }
 
-  return toReturn;
-}
+  // addEventListener
 
-// addEventListener
+  const addEvt = (target, type, listener) => {
 
-let addEvt = (target, type, listener) => {
+    if(typeof target === 'string'){ 
+
+      target = all(target);
+
+      for(element of target){
+        element.addEventListener(type, listener);
+      };
+    } else if (target instanceof NodeList || Array.isArray(target)){ 
+
+      for(element of target){
+        element.addEventListener(type, listener);
+      };
+    } else {
+
+      target.addEventListener(type, listener);
+    }
+
+  };
+
+  // DOMContentLoaded
+
+  const loaded = (listener) => {
+    addEvt(document, 'DOMContentLoaded', listener);
+  };
   
-  if(typeof target === 'string'){ 
-    
-    target = all(target);
-    
-    for(element of target){
-      element.addEventListener(type, listener);
-    };
-  } else if (target instanceof NodeList || Array.isArray(target)){ 
-    
-    for(element of target){
-      element.addEventListener(type, listener)
-    };
-  } else {
-    
-    target.addEventListener(type, listener);
-  }
+  //export
   
-};
-
-// DOMContentLoaded
-
-let loaded = (listener) => {
-  addEvt(document, 'DOMContentLoaded', listener);
-};
+  window.sel = sel;
+  window.all = all;
+  window.addEvt = addEvt;
+  window.loaded = loaded;
+  
+}());
