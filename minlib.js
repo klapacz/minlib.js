6ef2ef99b0cc7623c904c63f49d631075b400f41
kornel <lapacz.kornel@gmail.com>
2017-07-05 17:46:59 +0200

    event delegation

--- minlib.js
+++ minlib.js
@@ -16,6 +16,40 @@ window.queryAll = (selector, baseElement = document) => {
   return [selector];
 };
 
+// eventDelegation
+window.addDelegation = (parents, targets, type, listener) => {
+  
+  let check = (event) => {
+    let element = event.target;
+    
+    if(targets[0] === '.'){
+      let target = targets.slice(1, targets.length)
+      
+      for(let currentClass of element.classList){
+        if(currentClass == target){
+          listener(element, event)
+        }
+      }
+    } 
+    
+    if (targets[0] === '#'){
+      let target = targets.slice(1, targets.length)
+      
+      if(target = element.id){
+        listener(element, event)
+      }
+    }
+    
+    if(element.tagName.toLowerCase() === targets){
+      listener(element, event);
+    }
+  }
+  
+  for(let parent of queryAll(parents)){
+    parent.addEventListener(type, check, event);
+  }
+}
+
 // addEventListener
 window.addEvent = (targets, type, listener, useCapture = false) => {
   queryAll(targets).forEach(target => {
